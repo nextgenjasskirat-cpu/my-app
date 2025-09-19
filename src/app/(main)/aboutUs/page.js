7fa5a19be702d5d69484dc6f0a5e0ff22601d93c
Mahi singh <aka.mahisingh@gmail.com>
2025-09-19 12:53:16 +0530

    hi

--- src/app/(main)/aboutUs/page.js
+++ src/app/(main)/aboutUs/page.js
@@ -221,7 +221,7 @@ export default function AboutUsPage() {
 
 
 
-        {/* Map & Location Section */}
+        {/* Map & Location SectionS */}
         <section className="py-16">
           <div className="container mx-auto px-4">
             <h2 className="text-3xl font-bold text-center mb-4">
